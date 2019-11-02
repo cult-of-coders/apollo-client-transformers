d6e0eb60a087292b99f941ab474c212ebe5f1c29
Walter Steur <stewartrule@gmail.com>
2019-11-02 12:42:54 +0100

    Add compatibility with Websocket links

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import * as isPlainObject from 'is-plain-object';
-import { ApolloLink } from 'apollo-link';
-import { Transformers, TransformerMap } from './defs';
+import { ApolloLink, FetchResult, Observable } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
+import { DocumentNode } from 'graphql';
+import isPlainObject from 'is-plain-object';
+
+import { TransformerMap, Transformers } from './defs';
 
 export function parseObject(transformers: Transformers, object) {
   if (!object) {
@@ -34,14 +37,35 @@ export function parseObjectValues(map: TransformerMap, object) {
   }
 }
 
+export function isSubscription(query: DocumentNode) {
+  const definition = getMainDefinition(query);
+
+  return (
+    definition.kind === 'OperationDefinition' &&
+    definition.operation === 'subscription'
+  );
+}
+
+function parseResponse(response: FetchResult, transformers: Transformers) {
+  if (response.data) {
+    parseObject(transformers, response.data);
+  }
+
+  return response;
+}
+
 export function createTransformerLink(transformers: Transformers) {
   return new ApolloLink((operation, forward) => {
-    return forward(operation).map(response => {
-      if (response.data) {
-        parseObject(transformers, response.data);
-      }
+    if (isSubscription(operation.query)) {
+      return new Observable<FetchResult>(observer =>
+        forward(operation).subscribe(response =>
+          observer.next(parseResponse(response, transformers)),
+        ),
+      );
+    }
 
-      return response;
-    });
+    return forward(operation).map(response =>
+      parseResponse(response, transformers),
+    );
   });
 }
